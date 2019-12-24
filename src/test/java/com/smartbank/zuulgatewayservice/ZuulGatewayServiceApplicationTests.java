f3174547687855c2d92220a76cbfd5b9265070c3
jitu <jitu@gmail.com>
2019-12-24 14:31:09 +0530

    Initial Commit

---
+++ src/test/java/com/smartbank/zuulgatewayservice/ZuulGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbank.zuulgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
